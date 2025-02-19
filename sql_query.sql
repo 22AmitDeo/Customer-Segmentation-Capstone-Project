756b6dbee5b6f7ca97fbc6913b0d0b0198e935ec
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-02-20 01:21:11 +0530

    #

--- sql_query.sql
+++ sql_query.sql
@@ -21,7 +21,40 @@ WHERE `Customer ID` IS NULL
    
 -- No null values in the dataset
 
+-- Customer Segmentation
+SELECT `Customer ID`, `Total Spend`,  
+    CASE  
+        WHEN `Total Spend` > 1000 THEN 'High Spender'  
+        WHEN `Total Spend` BETWEEN 500 AND 1000 THEN 'Medium Spender'  
+        ELSE 'Low Spender'  
+    END AS Spend_Category  
 
--- Exporting the dataset
+FROM customer;
+
+SELECT `Customer ID`, `Days Since Last Purchase`,  
+    CASE  
+        WHEN `Days Since Last Purchase` < 15 THEN 'Frequent Buyer'  
+        WHEN `Days Since Last Purchase` BETWEEN 15 AND 30 THEN 'Average Buyer'  
+        ELSE 'Infrequent Buyer'  
+    END AS Buyer_Type  
+FROM customer;
+
+
+-- Top 10 Highest-Spending Customers
+SELECT `Customer ID`, `Total Spend`
+FROM customer
+ORDER BY `Total Spend` DESC
+LIMIT 10;
+
+
+-- Customers Who Haven’t Purchased in the Last 60 Days
+SELECT `Customer ID`, `Days Since Last Purchase`
+FROM customer
+WHERE `Days Since Last Purchase` > 60;
+
+
+
+
+
 
 
